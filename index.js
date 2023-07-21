edc6cc500268641a3b6ab6d411327af29f00c657
bbiomax <bbbiomax@gmail.com>
2023-07-22 00:16:24 +0300

    Добавил кодировку, убрал лишнее

--- index.js
+++ index.js
@@ -23,29 +23,6 @@ function chooseGameLevel() {
         levelNumberTwo.classList.remove('level_choosed');
         levelNumberThree.classList.add('level_choosed');
     })
-
-    // "checked" меняется только после нажатия на кнопку, из-за этого приходится кликать дважды, не знаю как исправить
-
-    // const levels = document.querySelectorAll('.levels_number');
-    // levels.forEach(level => {
-    //     level.addEventListener('click', () => {
-    //         for (let index = 0; index < radios.length; index++) {
-    //             if (radios[0].checked) {
-    //                 levelNumberOne.classList.add('level_choosed');
-    //                 levelNumberTwo.classList.remove('level_choosed');
-    //                 levelNumberThree.classList.remove('level_choosed');
-    //             } else if (radios[1].checked) {
-    //                 levelNumberOne.classList.remove('level_choosed');
-    //                 levelNumberTwo.classList.add('level_choosed');
-    //                 levelNumberThree.classList.remove('level_choosed');
-    //             } else if (radios[2].checked) {
-    //                 levelNumberOne.classList.remove('level_choosed');
-    //                 levelNumberTwo.classList.remove('level_choosed');
-    //                 levelNumberThree.classList.add('level_choosed');
-    //             }
-    //         }
-    //     })
-    // })
 }
 
 chooseGameLevel();
@@ -70,3 +47,5 @@ const startButton = document.querySelector('.start-button').addEventListener('cl
         window.location = './level.html';
     }
 })
+
+console.log(gameLevel);
