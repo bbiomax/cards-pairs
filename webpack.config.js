46b87507788e6287d3ba7c049c960b10a43b518b
bbiomax <bbbiomax@gmail.com>
2023-07-28 17:42:33 +0300

    точка выхода webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
+const path = require('path');
+
 module.exports = {
     entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            { text: /\.css$/, user: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
